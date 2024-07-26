6c1264696f1c6578debddbc2b7cdead660b8f632
Baekyohoon <qorygnsska@naver.com>
2024-07-26 18:04:51 +0900

    css

--- src/main/java/com/ss/cinema/controller/mypageController.java
+++ src/main/java/com/ss/cinema/controller/mypageController.java
@@ -1,5 +1,13 @@
 package com.ss.cinema.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class mypageController {
 
+	@RequestMapping("/myMovie")
+	public String myMovie() {
+		return "mypage/myMovie";
+	}
 }
