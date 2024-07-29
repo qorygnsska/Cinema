f2c787687ad61a7b17ca8349f431250788f5a6da
Hangma01 <dn09yooon@gmail.com>
2024-07-29 09:26:21 +0900

    mapper 수정

--- src/main/java/com/ss/cinema/controller/TicketController.java
+++ src/main/java/com/ss/cinema/controller/TicketController.java
@@ -1,5 +1,33 @@
 package com.ss.cinema.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ss.cinema.dto.CinemaDTO;
+import com.ss.cinema.dto.TheaterDTO;
+import com.ss.cinema.dto.movieDTO;
+import com.ss.cinema.service.TicketService;
+
+@Controller
 public class TicketController {
 
+	@Autowired
+	private TicketService ticketService;
+	
+	@RequestMapping("/")
+	public String ticketMenu(Model model, CinemaDTO cinemaDTO, TheaterDTO theaterDTO) {
+		System.out.println("TicketController ticketMenu");
+		
+		// 페이지 전환 시
+		List<movieDTO> movieList = ticketService.getMovieList(cinemaDTO);
+		
+		model.addAttribute("movieList",movieList);
+		
+		return "ticket/ticket";
+	}
+	
 }
--- src/main/java/com/ss/cinema/dto/CinemaDTO.java
+++ src/main/java/com/ss/cinema/dto/CinemaDTO.java
@@ -1,5 +1,18 @@
 package com.ss.cinema.dto;
 
-public class CinemaDTO {
+import java.util.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CinemaDTO {
+	private int cinemaNo;				// 영화관 No(PK)
+	private String cinemaRLG;			// 광역자치단체
+	private String cinemaBLG;			// 기초자치단체
+	private int cinemaMovieNo;			// 영화 No(RF)
+	private Date cinemaScreenDatee;		// 상영날짜
 }
