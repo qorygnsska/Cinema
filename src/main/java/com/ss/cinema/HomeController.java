a411cda6bd0fe2d55d7f82b19c5fc420118dd997
iwecs <bjsoon3333@gmail.com>
2024-07-30 17:48:27 +0900

    1

--- src/main/java/com/ss/cinema/HomeController.java
+++ src/main/java/com/ss/cinema/HomeController.java
@@ -21,6 +21,6 @@ public class HomeController {
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 
-		return "movie/movieDetail";
+		return "home";
 	}	
 }
