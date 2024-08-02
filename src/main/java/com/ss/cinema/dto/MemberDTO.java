7bd0838f7c83d50aee5af98ca2acdd7c5886f516
qorygnsska <qorygnsska@naver.com>
2024-08-02 15:16:39 +0900

    1

--- src/main/java/com/ss/cinema/dto/MemberDTO.java
+++ src/main/java/com/ss/cinema/dto/MemberDTO.java
@@ -12,12 +12,12 @@ public class MemberDTO {
 	private String memberId;		// 회원 ID(PK)
 	private String memberName;		// 회원 이름
 	private String memberPassword;	// 비번
-	private char memberGender;		// 성별
+	private String memberGender;		// 성별
 	private String mebmerJumin;		// 주민번호
 	private String memberPhone;		// 전화번호
 	private String memberEmail;		// 이메일
 	private int memberCoupon;		// 쿠폰
-	private char memberAdmin;		// 어드민 여부
+	private String memberAdmin;		// 어드민 여부
 	private int memberStamp;		// 스탬프
 
 }
