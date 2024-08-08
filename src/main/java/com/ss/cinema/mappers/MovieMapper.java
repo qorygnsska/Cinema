b24fc9bfd1b630f7db44d5a48de3da3251668eb3
qorygnsska <qorygnsska@naver.com>
2024-08-08 19:13:53 +0900

    moviemapper 어노테이션 추가

--- src/main/java/com/ss/cinema/mappers/MovieMapper.java
+++ src/main/java/com/ss/cinema/mappers/MovieMapper.java
@@ -2,8 +2,11 @@ package com.ss.cinema.mappers;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.ss.cinema.dto.movieDTO;
 
+@Mapper
 public interface MovieMapper {
 
 	// 영화 리스트 정보 가져오기
