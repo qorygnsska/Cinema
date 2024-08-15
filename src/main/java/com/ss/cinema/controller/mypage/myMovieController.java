a17eb07a14feb838380d5b5f7c1f7666245b1f3a
qorygnsska <qorygnsska@naver.com>
2024-08-15 23:58:47 +0900

    환불 구현

--- src/main/java/com/ss/cinema/controller/mypage/myMovieController.java
+++ src/main/java/com/ss/cinema/controller/mypage/myMovieController.java
@@ -1,9 +1,15 @@
 package com.ss.cinema.controller.mypage;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +19,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ss.cinema.dto.MemberDTO;
 import com.ss.cinema.dto.MyMovieDTO;
+import com.ss.cinema.key.appKey;
 import com.ss.cinema.service.mypage.myMovieService;
 import com.ss.cinema.service.mypage.myStampService;
 
@@ -66,8 +73,10 @@ public class myMovieController {
 		return "mypage/myMovie";
 	}
 	
+	
+	
 	@RequestMapping("/cancelticket")
-	public String cancelTicket(int tno, int pno, String seat, int tt, int ta, int ts, int theater, HttpSession session, RedirectAttributes redirectAttributes) {
+	public String cancelTicket(int tno, int pno, String seat, int tt, int ta, int ts, int theater, String uid, HttpSession session, RedirectAttributes redirectAttributes) {
 		
 		myMovieservice.deleteTicket(tno); // 티켓 지우기
 		myMovieservice.deletePayment(pno); // 결제내역 지우기
@@ -106,8 +115,61 @@ public class myMovieController {
 		 for(int row = 0; row < resultArray.length; row++) {
 			 	myMovieservice.deleteSeat(resultArray[row][0],resultArray[row][1], theater); // 좌석 삭제
 			 }
-		
-		redirectAttributes.addFlashAttribute("cancelMessage", "예매취소가 완료되었습니다!"); 
+		 
+		 
+		 // 결제 취소 api
+		 appKey key = new appKey();
+		 String imkey = key.getImport_restAPI_key();   // appKey 클래스에서 API 키 가져오기
+		 String imsecret = key.getImport_restAPI_secret(); // appKey 클래스에서 시크릿 키 가져오기
+		    
+		 // API 요청을 위한 JSON 문자열 생성
+		 String tokenBody = String.format("{\"imp_key\":\"%s\",\"imp_secret\":\"%s\"}", imkey, imsecret);
+		 String cancelBody = String.format("{\"imp_uid\":\"%s\",\"reason\":\"단순변심\"}", uid);
+
+		 // 토큰 요청
+		 HttpRequest token_request = HttpRequest.newBuilder()
+		         .uri(URI.create("https://api.iamport.kr/users/getToken"))
+		         .header("Content-Type", "application/json")
+		         .method("POST", HttpRequest.BodyPublishers.ofString(tokenBody))
+		         .build();
+		    
+		   
+
+		 // HttpClient를 사용해 요청 보내기
+		 HttpResponse<String> token_response;
+		 HttpResponse<String> cancel_response;
+		 try {
+			 // 토큰
+		     token_response = HttpClient.newHttpClient().send(token_request, HttpResponse.BodyHandlers.ofString());
+		     String token_responseBody = token_response.body();
+		        
+		     JSONParser parser = new JSONParser();
+		     JSONObject jsonObject = (JSONObject) parser.parse(token_responseBody);
+		     JSONObject responseObject = (JSONObject) jsonObject.get("response");
+		     String accessToken = (String) responseObject.get("access_token");  // 토큰 추출
+
+		     System.out.println("Access Token: " + accessToken);
+		        
+		     // 취소 요청		    
+			 HttpRequest cancel_request = HttpRequest.newBuilder()
+		        	 .uri(URI.create("https://api.iamport.kr/payments/cancel"))
+		        	 .header("Content-Type", "application/json")
+		        	 .header("Authorization", accessToken)
+		        	 .method("POST", HttpRequest.BodyPublishers.ofString(cancelBody))
+		        	 .build();
+			    
+		     cancel_response = HttpClient.newHttpClient().send(cancel_request, HttpResponse.BodyHandlers.ofString());
+		     String cancel_responseBody = cancel_response.body();
+		        
+		     System.out.println(cancel_responseBody);
+		     
+		     redirectAttributes.addFlashAttribute("cancelMessage", "결제 취소가 완료되었습니다!"); 
+		        
+		        
+		    } catch (Exception e) {
+		    	System.out.println("에러났어용!!");
+		        e.printStackTrace();
+		    }
 		
 		return "redirect:/myMovie";
 	}
